e94c1fb55b6ab9fe9787786ca9569b5be2e53905
chuang04 <chuang04@villanova.edu>
2023-12-07 10:33:44 -0500

    pt 1 liked button

--- api/animeData.js
+++ api/animeData.js
@@ -4,14 +4,16 @@ const animeData = [
       title: 'Attack on Titan',
       genre: 'Action, Drama, Fantasy',
       releaseDate: '2013-04-06',
-      synopsis: 'In a world where humanity resides within...'
+      synopsis: 'In a world where humanity resides within...',
+      likes: 0
     },
     {
       id: 2,
       title: 'My Hero Academia',
       genre: 'Action, Comedy, Super Power',
       releaseDate: '2016-04-03',
-      synopsis: 'In a world where people with superpowers (known as "quirks")...'
+      synopsis: 'In a world where people with superpowers (known as "quirks")...',
+      likes: 0
     },
   ];
   
