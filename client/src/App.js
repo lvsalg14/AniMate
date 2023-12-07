3d72dcc4e61c675168f1e5e096127f53952fd3e1
chuang04 <chuang04@villanova.edu>
2023-12-07 03:27:26 -0500

    Updated Site With Image and soeme other stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, FlatList } from "react";
-import logo from './logo.svg';
+import logo from './AniMate.png';
 import './App.css';
+
+
 
 class App extends Component {
   constructor(props) {
@@ -18,16 +20,34 @@ class App extends Component {
     this.callAPI();
   }
   
+  item = (
+    <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
+      
+    </div>  
+  );
 render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className = "App-title"> Welcome to React</h1>
-      </header>
+    
+    <body id="page-top">
+      <nav class = "navbar navbar-expanded-lg bg-secondary text-uppercase fixed-top" id= "mainNav">
+        <div class = "container">
+          <div class = "col-sm-2">
+            <img class = "img-fluid" src = {logo} href = "#page-top"/>
+          </div>
+        </div>
+      </nav>
+      <section class = "page-section" id="animeList">
+        <div>
+
+        </div>
+      </section>
       
-      <p className="App-intro">{this.state.apiResponse}</p>
-    </div>
+    </body>
+    
+
+
+
+      
   );
   }
 }
