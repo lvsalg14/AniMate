0004b1bd4ebcf3da65dfd064d0bab61d8de91293
lvsalg14 <lvsalgado14@gmail.com>
2023-12-07 13:04:32 -0500

    Demo

--- api/animeData.js
+++ api/animeData.js
@@ -3,7 +3,7 @@ const animeData = [
       id: 1,
       title: 'Attack on Titan',
       genre: 'Action, Drama, Fantasy',
-      releaseDate: '2013-04-06',
+      releaseDate: 'April 7, 2013',
       synopsis: 'Several hundred years ago, humans were nearly exterminated by titans. Titans are typically several stories tall, seem to have no intelligence, devour human beings and, worst of all, seem to do it for the pleasure rather than as a food source. A small percentage of humanity survived by walling themselves in a city protected by extremely high walls, even taller than the biggest of titans. Flash forward to the present and the city has not seen a titan in over 100 years. Teenage boy Eren and his foster sister Mikasa witness something horrific as the city walls are destroyed by a colossal titan that appears out of thin air. As the smaller titans flood the city, the two kids watch in horror as their mother is eaten alive. Eren vows that he will murder every single titan and take revenge for all of mankind.',
       photo: 'Attack_on_Titan_Season_1.webp'
     },
@@ -11,7 +11,7 @@ const animeData = [
       id: 2,
       title: 'My Hero Academia',
       genre: 'Action, Comedy, Adventure',
-      releaseDate: '2016-04-03',
+      releaseDate: 'April 3, 2016',
       synopsis: 'What would the world be like if 80 percent of the population manifested extraordinary superpowers called “Quirks” at age four? Heroes and villains would be battling it out everywhere! Becoming a hero would mean learning to use your power, but where would you go to study? U.A. Highs Hero Program of course! But what would you do if you were one of the 20 percent who were born Quirkless? Middle school student Izuku Midoriya wants to be a hero more than anything, but he hasnt got an ounce of power in him. With no chance of ever getting into the prestigious U.A. High School for budding heroes, his life is looking more and more like a dead end. Then an encounter with All Might, the greatest hero of them all gives him a chance to change his destiny…',
       photo: 'bx21459-DUKLgasrgeNO.jpg'
     },
@@ -19,7 +19,7 @@ const animeData = [
       id: 3,
       title: 'One Piece',
       genre: 'Action, Adventure, Comedy',
-      releaseDate: '1999-10-20',
+      releaseDate: 'October 20, 1999',
       synopsis: 'Gold Roger was known as the Pirate King, the strongest and most infamous being to have sailed the Grand Line. The capture and death of Roger by the World Government brought a change throughout the world. His last words before his death revealed the location of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece (which promises an unlimited amount of riches and fame), and quite possibly the most coveted of titles for the person who found it, the title of the Pirate King. Enter Monkey D. Luffy, a 17-year-old boy that defies your standard definition of a pirate. Rather than the popular persona of a wicked, hardened, toothless pirate who ransacks villages for fun, Luffys reason for being a pirate is one of pure wonder; the thought of an exciting adventure and meeting new and intriguing people, along with finding One Piece, are his reasons of becoming a pirate. Following in the footsteps of his childhood hero, Luffy and his crew travel across the Grand Line, experiencing crazy adventures, unveiling dark mysteries and battling strong enemies, all in order to reach One Piece.',
       photo: 'nx21-tXMN3Y20PIL9.jpg'
     },
@@ -27,7 +27,7 @@ const animeData = [
       id: 4,
       title: 'Naruto',
       genre: 'Action, Adventure, Fantasy',
-      releaseDate: '2002-10-03',
+      releaseDate: 'October 3, 2002',
       synopsis: 'Naruto Uzumaki, a hyperactive and knuckle-headed ninja, lives in Konohagakure, the Hidden Leaf village. Moments prior to his birth, a huge demon known as the Kyuubi, the Nine-tailed Fox, attacked Konohagakure and wreaked havoc. In order to put an end to the Kyuubis rampage, the leader of the village, the 4th Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto. Shunned because of the presence of the Kyuubi inside him, Naruto struggles to find his place in the village. He strives to become the Hokage of Konohagakure, and he meets many friends and foes along the way.',
       photo: 'bx20-YJvLbgJQPCoI.jpg'
     },
@@ -35,7 +35,7 @@ const animeData = [
       id: 5,
       title: 'Dragon Ball Z',
       genre: 'Action, Adventure, Comedy',
-      releaseDate: '1989-04-26',
+      releaseDate: 'April 26, 1989',
       synopsis: 'Goku is back with his new son, Gohan, but just when things are getting settled down, the adventures continue. Whether he is facing enemies such as Freeza, Cell, or Boo, Goku is proven to be an elite of his own and discovers his race, Saiyan. He meets many new people, gaining allies as well as enemies, as he still finds time to raise a family and be the happy-go-lucky Saiyan he is.',
       photo: 'bx813-QBIvCQgHcjcF.png'
     },  
